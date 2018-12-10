ab0bc907bbf46eb6da5338862ffc4b3bfb79dd3c
Siarhei Prystauka <siarhei_prystauka2@epam.com>
2018-12-10 17:00:47 +0300

    Add requestAnimationFrame polyfill

--- index.js
+++ index.js
@@ -104,3 +104,32 @@ function Kitty(name) {
 
 Kitty.prototype = Object.create(Cat.prototype);
 Kitty.prototype.constructor = Kitty;
+
+// *********** requestAnimationFrame polyfill ***********
+(() => {
+    const vendors = ['ms', 'moz', 'webkit', 'o'];
+    let lastTime = 0;
+
+    for(let x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[`${vendors[x]}RequestAnimationFrame`];
+        window.cancelAnimationFrame = window[`${vendors[x]}CancelAnimationFrame`] 
+                                   || window[`${vendors[x]}CancelRequestAnimationFrame`];
+    }
+
+    if (!window.requestAnimationFrame) {
+        window.requestAnimationFrame = (callback, element) => {
+            const currTime = new Date().getTime();
+            const timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            const id = window.setTimeout(
+                () => {callback(currTime + timeToCall);}, 
+                timeToCall
+              );
+
+            lastTime = currTime + timeToCall;
+
+            return id;
+        };
+    }
+
+    if (!window.cancelAnimationFrame) window.cancelAnimationFrame = id => {clearTimeout(id);};
+}());
