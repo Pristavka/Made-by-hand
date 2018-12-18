38eb6cbcd496ee2574f61fd52038849d2e67b5c0
Siarhei Prystauka <siarhei_prystauka2@epam.com>
2018-12-18 16:37:11 +0300

    Added Object.create polyfill

--- index.js
+++ index.js
@@ -12,6 +12,25 @@ const bind = function(fn, context) {
 };
 
 // *********** Object.create() implementation ***********
+if (typeof Object.create !== 'function') {
+    Object.create = function(o, props) {
+        function F() {}
+        F.prototype = o;
+   
+        if (typeof(props) === "object") {
+            for (prop in props) {
+                if (props.hasOwnProperty((prop))) {
+                    F[prop] = props[prop];
+                }
+            }
+        }
+
+        return new F();
+    };
+};
+
+// or
+
 const create = (proto, props) => {
     const newObject = {};
 
